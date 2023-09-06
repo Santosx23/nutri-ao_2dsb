529608c63779c5b31618da3f39d8d821e78740be
Santosx23 <130380761+Santosx23@users.noreply.github.com>
2023-09-06 12:24:01 -0300

    Add files via upload

--- nutricao-2dsb-main/introducao-javascript/js/principal.js
+++ nutricao-2dsb-main/introducao-javascript/js/principal.js
@@ -7,3 +7,20 @@ let titulo = document.querySelector(".titulo");
 console.log(titulo.textContent);
 //altera o conteudo da variavel titulo para o texto desejado
 titulo.textContent = "Fica Grande Nutrição";
+
+//mudanças na tabela
+let paciente = document .querySelector("#primeiro paciente");
+
+
+console.log(paciente);
+
+let tdPeso =  paciente.querySelector("info-peso");
+let tdAltura = paciente.querySelector("info-altura");
+
+let peso = tdPeso.textContent;
+let altura = tdAltura.textContent;
+
+let imc =peso / (altura*altura);
+console.log(imc)
+
+tdImc = paciente.querySelector
